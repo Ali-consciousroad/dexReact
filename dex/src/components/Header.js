388c755ce954df9fca44d6e8c746eb89aad5a156
ali-conscisousroad <ali_dindar@live.be>
2023-11-05 20:16:22 +0100

    Create header and related routes

--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import Logo from "../moralis-logo.svg";
+import Eth from "../eth.svg";
+import { Link } from  "react-router-dom";
 
 function Header() {
   return (
+    <header>
     <div>Header</div>
+    <div className="leftH">
+      <img src={Logo} alt="logo" className="logo" />
+      <Link to="/" className="Link">
+        <div className="headerItem">Swap</div>
+      </Link>
+      <Link to="/tokens" className="Link">
+        <div className="headerItem">Tokens</div>
+      </Link>
+    </div>
+    <div className="rightH">
+      <div className="headerItem">
+        <img src={Eth} alt="eth" className="eth" />
+        Ethereum
+      </div>
+      <div className="connectButton">Connect</div>
+    </div>
+    </header>
   )
 }
 
